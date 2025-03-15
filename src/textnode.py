5ea9906d118df877674904fe7ca5ef7a0447e192
stichj <jonas.stich@tum.de>
2025-03-15 14:16:05 +0100

    Implement TextType enum, TextNode class, overwrite equals and string representation function of TextNode class

---
+++ src/textnode.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+class TextType(Enum):
+    BOLD = "bold_text"
+    ITALIC = "italic_text"
+    CODE = "code_text"
+    LINK = "link"
+    IMAGE = "image"
+    
+class TextNode():
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+        
+    def __eq__(self, other):
+        if self.text == other.text and \
+            TextType(self.text_type) == TextType(other.text_type) and \
+            self.url == other.url \
+        :
+            return True
+        return False
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+        
