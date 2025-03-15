bc834ad1ae6ed48efeff0c6710cf9fce24a87bdc
stichj <jonas.stich@tum.de>
2025-03-15 14:43:23 +0100

    Add test cases for TextNode class

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -7,6 +7,23 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
+        
+        node3 = TextNode("This is a text node", TextType.BOLD, None)
+        self.assertEqual(node, node3)
+        
+        node.url = "https://www.boot.dev"
+        node2.url = "https://www.boot.dev"
+        self.assertEqual(node, node2)
+        
+        node4 = TextNode("This is a text node ", TextType.BOLD, "https://www.boot.dev")
+        self.assertNotEqual(node, node4)
+        
+        node.url = None
+        node5 = TextNode("This is a text node", TextType("bold"))
+        self.assertEqual(node, node5)
+        
+        node5.text_type = TextType.ITALIC
+        self.assertNotEqual(node, node5)
 
 
 if __name__ == "__main__":
